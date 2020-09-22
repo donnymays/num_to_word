92953f4d9644ffb061e1faa9588dcf776e9329ae
Alma <a.q.frankenstein@gmail.com>
2020-09-22 13:11:30 -0700

    work for single digits and teens

--- lib/num_to_word.rb
+++ lib/num_to_word.rb
@@ -1,7 +1,10 @@
-class Number
+require('pry')
+
+class Number  
   def initialize(number)
     @number = number
-    @ones = {"1"=>"one", "2"=>"two", "3"=>"three", "4"=>"four", "5"=>"five", "6"=>"six", "7"=>"seven", "8"=>"eight", "9"=>"nine"}
+    @ones = {1=>"one", 2=>"two", 3=>"three", 4=>"four", 5=>"five", 6=>"six", 7=>"seven", 8=>"eight", 9=>"nine"}
+    @teens = {10=>"ten", 11=>"eleven", 12=>"twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen"}
   end
 
   def number
@@ -11,15 +14,20 @@ class Number
   def ones
     @ones
   end
+  
+  def ones_number_to_word()
+    as_words = (@ones.fetch(@number))
+  end  
 
-  def number_to_word()
-    as_words = []
-    number_array = @number.split("")
-    number_array.each do |element|
-      word = @ones.fetch(element)
-      as_words.push(word)
-    end
-    as_words.join(" ")
+  def teens_number_to_word()
+    as_words = (@teens.fetch(@number))
   end
-end
 
+  def number_to_word() 
+    if @number < 10
+      ones_number_to_word()
+    elsif (@number >= 10 && @number < 20)
+      teens_number_to_word
+    end
+  end
+end 
--- spec/num_to_word_spec.rb
+++ spec/num_to_word_spec.rb
@@ -2,10 +2,18 @@ require ('num_to_word')
 require('rspec')
 
 describe(Number) do
-  describe("#number_to_word") do
-    it('returns the english name of an imputted integer') do
-      numblur = Number.new("3")
+ 
+  describe("#ones_number_to_word") do
+    it('returns the english name of a single digit') do
+      numblur = Number.new(3)
       expect(numblur.number_to_word()).to(eq("three"))
+    end
+  end
+
+  describe("#teens_number_to_word") do
+    it('returns english name of an integer between 10 and 19') do
+      numblur = Number.new(14)
+      expect(numblur.number_to_word()).to(eq("fourteen"))
     end
   end
 end
